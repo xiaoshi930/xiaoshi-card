aaa28235533e474e8e6d62d549eee817ae50734a
xiaoshi <115949669+xiaoshi930@users.noreply.github.com>
2025-07-24 22:51:38 +0800

    Update xiaoshi-config.js

--- xiaoshi-config.js
+++ xiaoshi-config.js
@@ -1,77 +1,77 @@
 export const cardConfigs = [
   {
     type: 'xiaoshi-climate-card',
-    name: '消逝卡片组 空调卡',
-    description: '加载空调'
+    name: '消逝卡片组-空调卡',
+    description: '空调卡'
   },
   {
     type: 'xiaoshi-computer-card',
-    name: '消逝卡片组 空调卡',
-    description: '加载空调'
+    name: '消逝卡片组-电脑卡',
+    description: '电脑卡'
   },
   {
     type: 'xiaoshi-light-card',
-    name: '消逝卡片组 灯光卡',
-    description: '加载灯光'
+    name: '消逝卡片组-灯光卡',
+    description: '灯光卡'
   },
   {
     type: 'xiaoshi-switch-card',
-    name: '消逝卡片组 插座卡',
-    description: '加载插座'
+    name: '消逝卡片组-插座卡',
+    description: '插座卡'
   },
   {
     type: 'xiaoshi-text-card',
-    name: '消逝卡片组 输入卡',
+    name: '消逝卡片组-输入卡',
     description: 'text.实体输入框'
   },
   {
     type: 'xiaoshi-video-card',
-    name: '消逝卡片组 视频卡',
+    name: '消逝卡片组-视频卡',
     description: '加载api视频背景'
   },  
   {
     type: 'xiaoshi-image-card',
-    name: '消逝卡片组 图片卡',
+    name: '消逝卡片组-图片卡',
     description: '加载api图片背景'
   },
   {
     type: 'xiaoshi-time-card',
-    name: '消逝卡片组 时间卡',
+    name: '消逝卡片组-时间卡',
     description: '显示时间（需要配合万年历NR）'
   },
   {
     type: 'xiaoshi-grid-card',
-    name: '消逝卡片组 分布卡',
+    name: '消逝卡片组-分布卡',
     description: '房间温度分布、湿度分布卡片'
   },
   {
     type: 'xiaoshi-slider-card',
-    name: '消逝卡片组 进度条',
+    name: '消逝卡片组-进度条',
     description: '进度条'
   },
   {
     type: 'xiaoshi-state-grid-phone',
-    name: '消逝卡片组 国网卡片-日历手机端',
+    name: '消逝卡片组-国网卡片-日历手机端',
     description: '国网信息卡（需要配合国网NR）'
   },
   {
     type: 'xiaoshi-state-grid-pad',
-    name: '消逝卡片组 国网卡片-日历平板端',
+    name: '消逝卡片组-国网卡片-日历平板端',
     description: '国网信息卡（需要配合国网NR）'
   },
   {
     type: 'xiaoshi-lunar',
-    name: '消逝卡片组 万年历日历',
+    name: '消逝卡片组-万年历日历',
     description: '万年历日历（需要配合万年历NR）'
   },
   {
     type: 'xiaoshi-lunar-phone',
-    name: '消逝卡片组 万年历手机端',
+    name: '消逝卡片组-万年历手机端',
     description: '万年历手机端（需要配合万年历NR）'
   },
   {
     type: 'xiaoshi-lunar-pad',
-    name: '消逝卡片组 万年历平板端',
+    name: '消逝卡片组-万年历平板端',
     description: '万年历平板端（需要配合万年历NR）'
   },
 ];
